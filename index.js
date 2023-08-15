5de9fdf0c686340b23eb31341583c3fd47afc920
Deepak <deepakimandi@gmail.com>
2023-08-15 14:06:57 +0530

    Refactored code, added comments and error handling

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config(); 
 const express = require('express');
-const { Sequelize } = require('sequelize');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 const sequelize = require('./db'); 
 
 // Create Express app
@@ -15,54 +13,65 @@ app.use(bodyParser.json());
 // Define routes and logic in separate functions
 app.post('/identify', identifyContact);
 
+// Core logic to identify related contacts
 async function identifyContact(req, res) {
-  const { email, phoneNumber } = req.body;
+  try {
+    const { email, phoneNumber } = req.body;
 
-  let primaryContactId = null;
-  let emails = [];
-  let phoneNumbers = [];
-  let secondaryContactIds = [];
+    let primaryContactId = null;
+    let emails = [];
+    let phoneNumbers = [];
+    let secondaryContactIds = [];
+    
+    if (!email && !phoneNumber) {
+      return res.status(400).json({ error: 'Email or phone number is required.' });
+    }
 
-  if (email || phoneNumber) {
-    const emailPrimaryContact = await findPrimaryContact('email', email);
-    const phonePrimaryContact = await findPrimaryContact('phoneNumber', phoneNumber);
+    // Retrieve the primary records corresponding to the request email and phoneNumber
+    const emailPrimaryContact = email ? await findPrimaryContact('email', email) : null;
+    const phonePrimaryContact = phoneNumber ? await findPrimaryContact('phoneNumber', phoneNumber) : null;
   
     const isOneNull = (!email !== !phoneNumber);
     const isNewContact = !emailPrimaryContact && !phonePrimaryContact;
     const isSecondaryContact = !emailPrimaryContact || !phonePrimaryContact;
 
-    if (isOneNull) {
+    if (isOneNull) { // Check if exactly one of the request fields is NULL
       primaryContactId = emailPrimaryContact ? emailPrimaryContact.id : phonePrimaryContact ? 
         phonePrimaryContact.id : null;
-    } else if (isNewContact) {
+    } else if (isNewContact) { // Check if a new primary contact has to be created
       primaryContactId = await createNewContact(email, phoneNumber, null, 'primary');
-    } else if (isSecondaryContact) {
+    } else if (isSecondaryContact) { // Check if a new secondary contact has to be created
       primaryContactId = emailPrimaryContact ? emailPrimaryContact.id : phonePrimaryContact.id;
       await createNewContact(email, phoneNumber, primaryContactId, 'secondary');
-    } else if (emailPrimaryContact.id !== phonePrimaryContact.id) {
+    } else if (emailPrimaryContact.id !== phonePrimaryContact.id) { // Check if a primary has to be turned to secondary
       primaryContactId = await updateContacts(emailPrimaryContact, phonePrimaryContact);
     } else {
       primaryContactId = emailPrimaryContact.id;
     }
   
-    const relatedContacts = await findRelatedContacts(primaryContactId);
-  
-    emails = [...new Set(relatedContacts.map(item => item.email))];
-    phoneNumbers = [...new Set(relatedContacts.map(item => item.phoneNumber))];
-    secondaryContactIds = relatedContacts.filter(item => item.id !== primaryContactId).map(item => item.id);
+    if(primaryContactId) { // Retrieve the contacts related to the primaryContactId 
+      const relatedContacts = await findRelatedContacts(primaryContactId);
+      emails = [...new Set(relatedContacts.map(item => item.email))];
+      phoneNumbers = [...new Set(relatedContacts.map(item => item.phoneNumber))];
+      secondaryContactIds = relatedContacts.filter(item => item.id !== primaryContactId).map(item => item.id);
+    }
+
+    const response = {
+      contact: {
+        primaryContactId,
+        emails,
+        phoneNumbers,
+        secondaryContactIds,
+      },
+    };
+    res.json(response);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An internal error occurred.' });
   }
-  
-  const response = {
-    contact: {
-      primaryContactId,
-      emails,
-      phoneNumbers,
-      secondaryContactIds,
-    },
-  };
-  res.json(response);
 }
 
+// Function to retrieve the primary contact record
 async function findPrimaryContact(field, value) {
   const query = `
     SELECT *
@@ -78,6 +87,7 @@ async function findPrimaryContact(field, value) {
     )
     AND deletedAt IS NULL;
   `;
+
   const result = await sequelize.query(query, {
     replacements: { value },
     type: sequelize.QueryTypes.SELECT
@@ -85,6 +95,7 @@ async function findPrimaryContact(field, value) {
   return result[0];
 }
 
+// Function to insert a new contact
 async function createNewContact(email, phoneNumber, linkedId, linkPrecedence) {
   const newContact = {
     email,
@@ -97,6 +108,7 @@ async function createNewContact(email, phoneNumber, linkedId, linkPrecedence) {
     INSERT INTO Contact (email, phoneNumber, linkedId, linkPrecedence)
     VALUES (:email, :phoneNumber, :linkedId, :linkPrecedence);
   `;
+
   const [result] = await sequelize.query(insertQuery, {
     replacements: newContact,
     type: sequelize.QueryTypes.INSERT
@@ -104,6 +116,7 @@ async function createNewContact(email, phoneNumber, linkedId, linkPrecedence) {
   return result;
 }
 
+// Function to turn primary to secondary and relink appropriately based on the creation time
 async function updateContacts(emailPrimaryContact, phonePrimaryContact) {
   const olderContactId = emailPrimaryContact.createdAt > phonePrimaryContact.createdAt ?
     phonePrimaryContact.id : emailPrimaryContact.id;
@@ -123,6 +136,7 @@ async function updateContacts(emailPrimaryContact, phonePrimaryContact) {
   return olderContactId;
 }
 
+// Function to retrieve the primary contact record
 async function findRelatedContacts(primaryContactId) {
   const queryFinal = `
     SELECT id, email, phoneNumber
